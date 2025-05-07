47a2ba0b1d3a90c600a47192d97ef83efe75d345
chiarasanasi <chiarasanasi.work@gmail.com>
2025-05-07 17:10:59 +0200

    update esercizio 1

---
+++ src/Esercizio1/MainEsercizio1.java
@@ -0,0 +1,11 @@
+package Esercizio1;
+
+public class MainEsercizio1 {
+    public static void main(String[] args) {
+        Rettangolo rettangolo1 = new Rettangolo(5,4);
+        Rettangolo rettangolo2 = new Rettangolo(8,13);
+
+
+        rettangolo1.stampaDueRettangoli(rettangolo1,rettangolo2);
+    }
+}
--- src/Esercizio1/Rettangolo.java
+++ src/Esercizio1/Rettangolo.java
@@ -1,13 +1,7 @@
 package Esercizio1;
 
 public class Rettangolo {
-    public static void main(String[] args) {
-        Rettangolo rettangolo1 = new Rettangolo(5,4);
-        Rettangolo rettangolo2 = new Rettangolo(8,13);
 
-
-        rettangolo1.stampaDueRettangoli(rettangolo1,rettangolo2);
-    }
     private final double altezza;
     private final double larghezza;
 
