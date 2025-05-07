591cb77a61a5bbba34c1e5180aa301a0d5dc2e3c
chiarasanasi <chiarasanasi.work@gmail.com>
2025-05-07 17:20:38 +0200

    Update esercizio 1

--- src/Esercizio1/MainEsercizio1.java
+++ src/Esercizio1/MainEsercizio1.java
@@ -1,11 +1,32 @@
 package Esercizio1;
 
 public class MainEsercizio1 {
+
+    public static void stampaRettangolo(Rettangolo r1){
+        System.out.println("il perimetro del rettangolo è " + r1.calcoloPerimetro());
+        System.out.println("l'area del rettangolo è " + + r1.calcoloArea());
+
+    }
+
+    public static void stampaDueRettangoli(Rettangolo r1, Rettangolo r2){
+        System.out.println("Rettangolo 1 : ");
+        stampaRettangolo(r1);
+
+        System.out.println("Rettangolo 2 : ");
+        stampaRettangolo(r2);
+
+        double sommaAree = r1.calcoloArea() + r2.calcoloArea();
+        double sommaPerimetri = r1.calcoloPerimetro() + r2.calcoloPerimetro();
+
+        System.out.println("Somma dei perimetri : " + sommaPerimetri);
+        System.out.println("Somma delle aree : " + sommaAree);
+    }
+
     public static void main(String[] args) {
         Rettangolo rettangolo1 = new Rettangolo(5,4);
         Rettangolo rettangolo2 = new Rettangolo(8,13);
 
+        stampaDueRettangoli(rettangolo1,rettangolo2);
 
-        rettangolo1.stampaDueRettangoli(rettangolo1,rettangolo2);
     }
 }
--- src/Esercizio1/Rettangolo.java
+++ src/Esercizio1/Rettangolo.java
@@ -21,25 +21,4 @@ public class Rettangolo {
 
 
 
-    public void stampaRettangolo(){
-        System.out.println("il perimetro del rettangolo è " + calcoloPerimetro());
-        System.out.println("l'area del rettangolo è " + calcoloArea());
-
-    }
-
-    public void stampaDueRettangoli(Rettangolo r1, Rettangolo r2){
-        System.out.println("Rettangolo 1 : ");
-        r1.stampaRettangolo();
-
-        System.out.println("Rettangolo 2 : ");
-        r2.stampaRettangolo();
-
-        double sommaAree = r1.calcoloArea() + r2.calcoloArea();
-        double sommaPerimetri = r1.calcoloPerimetro() + r2.calcoloPerimetro();
-
-        System.out.println("Somma dei perimetri : " + sommaPerimetri);
-        System.out.println("Somma delle aree : " + sommaAree);
-    }
-
-
 }
